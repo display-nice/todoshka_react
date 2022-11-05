07dda260c16be359e8844033ad8c9db37a65a830
Oleg Grigorev <osrolex@yandex.ru>
2022-11-05 13:01:25 +0500

    + component 'search-panel'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import AppHeader from '../app-header/app-header';
+import SearchPanel from '../search-panel/search-panel';
+import React from 'react';
 
 function App() {
   return (
-    <AppHeader/>
+    <div className="app">
+      <AppHeader/>
+      <div className='search-panel d-flex'>
+        <SearchPanel/>
+      </div>
+    </div>
   );
 }
 
